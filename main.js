017d86a707cce7a4c3a52a65681924e9034d3d6d
Hakan Tekın <ahmethakantekin14@gmail.com>
2020-07-12 16:59:58 +0300

    Project card templates, Card layout for experience and school

---
+++ main.js
@@ -0,0 +1,11 @@
+document.getElementById("github").onclick = ()=>{
+    window.open("https://github.com/hakanTekin", "_blank");
+};
+document.getElementById("linkedIn").onclick = ()=>{
+    window.open("https://www.linkedin.com/in/hakan-tekin", "_blank");
+}
+
+function createCardCover(event) {
+    var element = event.target;
+    
+}
