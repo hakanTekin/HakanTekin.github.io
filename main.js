6357a51bbc348a28b6793abe6f58cf679b247fe4
Hakan Ahmet Tekin <hakan.ahmet.tekin@gmail.com>
2020-07-19 10:46:51 +0300

    week 2

--- main.js
+++ main.js
@@ -1,11 +1,52 @@
-document.getElementById("github").onclick = ()=>{
-    window.open("https://github.com/hakanTekin", "_blank");
+document.getElementById("github").onclick = () => {
+
 };
-document.getElementById("linkedIn").onclick = ()=>{
+document.getElementById("linkedIn").onclick = () => {
     window.open("https://www.linkedin.com/in/hakan-tekin", "_blank");
 }
 
-function createCardCover(event) {
-    var element = event.target;
-    
+
+document.onload = start();
+
+function start() {
+    var videoElementParents = document.getElementsByClassName("card-large");
+    for (var item of videoElementParents) {
+        item.onmouseover = () => {
+            var videos = item.getElementsByTagName("video");
+            for (var v of videos) {
+                v.play();
+            }
+        };
+
+        item.onmouseout = () => {
+            var videos = item.getElementsByTagName("video");
+            for (var v of videos) {
+                v.pause();
+                v.currentTime = 0;
+            }
+        };
+
+    }
+}
+
+function createCard(imagePath, cardClass, coverClass, titleClass, subtitleClass, cardTitle, cardSubtitle, sectionName) {
+    var cardDiv = document.createElement("div");
+    cardDiv.style.backgroundImage = imagePath;
+    cardDiv.className = cardClass;
+
+    var coverDiv = document.createElement("div");
+    coverDiv.className = coverClass;
+    var cardTitleP = document.createElement("p");
+    cardTitleP.className = titleClass;
+    cardTitleP.innerHTML = cardTitle;
+
+    var cardSubtitleP = document.createElement("p");
+    cardSubtitleP.className = subtitleClass;
+    cardSubtitleP.innerHTML = cardSubtitle;
+
+    coverDiv.appendChild(cardTitleP);
+    coverDiv.appendChild(cardSubtitleP);
+    cardDiv.appendChild(coverDiv);
+
+    document.getElementById(sectionName).appendChild(cardDiv);
 }
