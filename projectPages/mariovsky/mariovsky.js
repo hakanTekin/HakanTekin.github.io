f0bcb083e25d8d2085388c75133e4c681336525f
HakanTekin <ahmethakantekin14@gmail.com>
2020-11-02 16:53:10 +0300

    Video sizes changed, start/stop

--- projectPages/mariovsky/mariovsky.js
+++ projectPages/mariovsky/mariovsky.js
@@ -1,3 +1,15 @@
 document.getElementById("github").onclick = () => {
     window.open("https://github.com/hakanTekin/Vladimir-Mariovsky", "_blank");
 };
+
+var isPlaying = true;
+const videowrappers = document.getElementsByTagName('videowrapper');
+
+for (let index = 0; index < videowrappers.length; index++) {
+    const element = videowrappers[index];
+    element.onclick = () =>{
+        var x = element.getElementsByTagName('video')[0];
+        x.paused ? x.play() : x.pause(); 
+    }
+        
+}
